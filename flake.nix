a9cfe06a3a129defbffae677416f115472c1549f
Sylvain Navers - Personnel <sylvain.navers@gmail.com>
2024-01-08 19:46:35 +0100

    Feat: First commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Sylvain's nixos global flake configuration";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-23.11";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ]; 
+      };
+    };
+
+  };
+}
